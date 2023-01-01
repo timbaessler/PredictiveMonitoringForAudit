3aa451ee657f99da6ad25251beb109e4430f18fa
Tim Bäßler <94218704+timbaessler@users.noreply.github.com>
2023-01-01 08:29:40 +0100

    Update cv_models.py

--- src/models/cv_models.py
+++ src/models/cv_models.py
@@ -5,13 +5,14 @@ from sys import platform
 
 class CrossValidation:
 
-    def __init__(self, classifier: str, param_dict: dict, categorical=False, gpu=False, cvs=3, random_state=42):
+    def __init__(self, classifier: str, param_dict: dict, tree_method='hist', categorical=False, gpu=False, cvs=3, random_state=42):
         self.classifier = classifier
         self.param_dict = param_dict
         self.random_state = random_state
         self.cvs = cvs
         self.gepu = gpu
         self.categorical = categorical
+        self.tree_method = tree_method
 
     def get_classifier(self):
         if self.classifier == "XGBoost":
@@ -28,7 +29,8 @@ class CrossValidation:
                                    verbose=10)
             else:
                 clf = GridSearchCV(XGBClassifier(random_state=self.random_state,
-                                                 eval_metric="auc"),
+                                                 eval_metric="auc",
+                                                tree_method=self.tree_method),
                                    param_grid=self.param_dict,
                                    scoring='roc_auc',
                                    refit=True,
